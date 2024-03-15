0deb146dc44e2f1489d98668b15f222dd936d2d4
avni <avniiiiiii07@gmail.com>
2024-03-15 17:44:00 +0530

    yes

--- system.cpp
+++ system.cpp
@@ -5,4 +5,9 @@
 using namespace std;
 int main()
 {
+    int a, i = 0; // two integer variables a and i, where a is uninitialized and i is initialized to 0.
+
+    string text, old, password1, password2, pass, name, password0, user, age, word, word1; // we'll use these variables later on
+    string creds[2], cp[2];
+    // create an interface which will show 4 options://
 }
