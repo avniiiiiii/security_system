1f31683a6f3dc635ebcb79c5a5a4b45b0986dc28
avni <avniiiiiii07@gmail.com>
2024-03-15 17:49:59 +0530

    commit

--- system.cpp
+++ system.cpp
@@ -10,4 +10,12 @@ int main()
     string text, old, password1, password2, pass, name, password0, user, age, word, word1; // we'll use these variables later on
     string creds[2], cp[2];
     // create an interface which will show 4 options://
+    cout << "╔══════════════════════════════════╗" << endl;
+    cout << "║          Security System          ║" << endl;
+    cout << "╠══════════════════════════════════╣" << endl;
+    cout << "║ 1. Register                       ║" << endl;
+    cout << "║ 2. Login                          ║" << endl;
+    cout << "║ 3. Change Password                ║" << endl;
+    cout << "║ 4. End Program                    ║" << endl;
+    cout << "╚══════════════════════════════════╝" << endl;
 }
